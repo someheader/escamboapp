563761ef40ccd5018fe65adf94033dffb0da9316
someheader <willinakamura@gmail.com>
2018-09-22 23:12:08 -0300

    Changed parameters in admin.rb, testing sublime gitshavvy

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,12 @@
 class Admin < ApplicationRecord
-  enum role: [:full_access, :restricted_access]
+  enum role: [
+    :full_access,
+    :restricted_access
+  ]
+
+  scope :with_full_access, -> { where(role: 0) }
+  scope :with_restricted_access, -> { where(role: 1) }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
